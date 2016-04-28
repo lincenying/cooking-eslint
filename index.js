7cd1cee77f38404c3d5f9d71d59ad760ba5aac5f
qingwei.li <cinwell.li@gmail.com>
2016-04-28 17:00:49 +0800

    add extend

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 module.exports = function (cooking) {
   cooking.add('preLoader.js', {
     test: /\.jsx?$/,
+    exclude: /node_modules|bower_components/,
     loaders: ['eslint']
   })
 
   cooking.add('preLoader.vue', {
     test: /\.vue$/,
+    exclude: /node_modules|bower_components/,
     loaders: ['eslint']
   })
 }
